71fb1b6c7f7124872bb0e4196e241276513fa013
kholaazeem104@gmil.com <azkaazeem804@gmail.com>
2025-10-19 06:42:02 +0500

    signup page complete

--- auth.js
+++ auth.js
@@ -1,1 +1,48 @@
-import supabase from "config.js"
+import supabase from "/config.js"
+
+let signupPage = document.getElementById("signup-page")
+let signupEmail = document.getElementById("signup-email")
+let signupPassword = document.getElementById("signup-password")
+let signupPhone = document.getElementById("signup-phone#")
+let signupName = document.getElementById("signup-name")
+
+async function signup(e) {
+    e.preventDefault()
+    try {
+        if (!signupEmail.value) {
+            alert("Please Enter your Email")
+            return
+        }
+        if (!signupPassword.value) {
+            alert("Please Enter your Password")
+            return
+        }
+        const { data, error } = await supabase.auth.signUp(
+            {
+                email: signupEmail.value,
+                password: signupPassword.value,
+                options: {
+                    data: {
+                        name: signupName.value,
+                        phone: signupPhone.value,
+                    }
+                }
+            }
+        )
+        if (error) {
+            console.log(error)
+        } else {
+            location.href = "home.html";
+        }
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+signupPage.addEventListener("submit", signup)
+
+
+
+       /////////////login page
+
--- config.js
+++ config.js
@@ -4,4 +4,6 @@ console.log(createClient);
 const SUPABASE_URL ="https://xvkaogsogujaypobdhqb.supabase.co";
 const SUPABASE_ANNON_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2a2FvZ3NvZ3VqYXlwb2JkaHFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3MzQ1OTEsImV4cCI6MjA3NjMxMDU5MX0.Ly5ivveif0bpGdpPhnASLP9JArsqdrehHmEHVSkUnAM";
 
-const supabase = createClient(SUPABASE_URL , SUPABASE_ANNON_KEY)
+const SUPABASE = createClient(SUPABASE_URL , SUPABASE_ANNON_KEY)
+
+export default SUPABASE
