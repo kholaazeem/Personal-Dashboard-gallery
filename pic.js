48e0b41874f2487a21011b26a0df4e7a6d987726
khola azeem <kholaazeem104@gmail.com>
2025-12-07 15:27:14 +0500

    session storage: upload iamge and getpublic url done

--- pic.js
+++ pic.js
@@ -3,16 +3,16 @@ import supabase from "./config.js"
 
 
 let inputFile = document.getElementById("input-file");
-console.log(inputFile.files);
-
 let uplBtn = document.getElementById("upl-btn");
-console.log(uplBtn)
+let image = document.getElementById("image")
+let pubUrl;
+
 
 async function uplFile() {
     // console.log(inputFile.files[0].name);
     let file = inputFile.files[0];
-    let fileName = `${Date.now()} ${file.name} `
-    //    console.log(file,fileName)
+    let fileName = `${Date.now()} - ${file.name} `
+    console.log(file, fileName)
 
     try {
 
@@ -25,41 +25,24 @@ async function uplFile() {
             })
 
         if (data) {
-            console.log(`Data inert succesfuly: ${data}`)
+            console.log(data)
+            console.log(data.fullPath.split('/'))
+            var fileData = data.fullPath.split('/')   //split function array form  bna daita ha and sb ko index pr rkh daita ha
+            console.log(fileData)
+            console.log(fileData[1])
+            pubUrl = fileData[1]
 
-              const { data : {urlData} } = supabase
+            const { data:  urlData } = supabase
                 .storage
                 .from('pictures')
-                .getPublicUrl(fileName)
-
-               
-                           console.log("Public URL:", urlData.publicUrl);
-                
+                .getPublicUrl(pubUrl)
+                if(urlData){
+                    console.log(urlData.publicUrl)
+                    image.src = urlData.publicUrl
+                }
         } else {
-            console.log(`Supabase Error: ${error}`)
-          
+            console.log(error)
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
