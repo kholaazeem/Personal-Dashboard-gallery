1793eca8f4e462b1c53d2d36515b0dd5dd887b26
khola azeem <kholaazeem104@gmail.com>
2025-12-07 06:46:56 +0500

    change authclient url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-import {createClient} from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
+import {createClient} from "https://esm.sh/@supabase/supabase-js" 
 console.log(createClient);
 
 const SUPABASE_URL ="https://xvkaogsogujaypobdhqb.supabase.co";
